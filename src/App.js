02a7f4b1f7d92248fd9d687b2c8e7fb1d2c2e916
Eli Codes <elicodes@Elis-Mac-mini.local>
2021-01-07 22:35:29 -0500

    Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/header";
+import Headline from "./components/headline";
 import "./app.scss";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <section className="main">
+        <Headline header="Posts" desc="Click the button to render posts" />
+      </section>
     </div>
   );
 };
