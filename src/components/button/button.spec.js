0226c92d3d062c08923230d39c3ee811579e9e4a
Eli Codes <elicodes@Elis-Mac-mini.local>
2021-01-09 20:43:38 -0500

    simulate click event on button component

--- src/components/button/button.spec.js
+++ src/components/button/button.spec.js
@@ -17,10 +17,12 @@ describe("SharedButton Component", () => {
 
   describe("Renders", () => {
     let wrapper;
+    let mockFunc;
     beforeEach(() => {
+      mockFunc = jest.fn();
       const props = {
         buttonText: "Example Button Test",
-        emitEvent: () => {},
+        emitEvent: mockFunc,
       };
       wrapper = shallow(<SharedButton {...props} />);
     });
@@ -28,6 +30,13 @@ describe("SharedButton Component", () => {
     it("should render a button", () => {
       const button = findByTestAttr(wrapper, "buttonComponent");
       expect(button.length).toBe(1);
+    });
+
+    it("should emit callback on click event", () => {
+      const button = findByTestAttr(wrapper, "buttonComponent");
+      button.simulate("click");
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1);
     });
   });
 });
